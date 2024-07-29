c3e6ef693bd8b9e141bc059183dd2cf9b78be826
Pankesh Meena <112115701+PankeshMeena1@users.noreply.github.com>
2024-07-30 02:19:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-// const express = require('express');
-// const app = express();
-// const bodyParser = require('body-parser');
-// const dotenv = require('dotenv');
-// const cors = require('cors');
-// const userRoutes = require('./routes/user.routes');
-// const taskRoutes = require('./routes/task.routes');
-// dotenv.config();
 
-// app.use(express.json());
-// app.use(bodyParser.json());
 
-// app.use('/api/users', userRoutes);
-// app.use('/api/tasks', taskRoutes);
-
-// const PORT = process.env.PORT || 5000;
-require('dotenv').config();
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const userRoutes = require('./routes/user.routes');
 const taskRoutes = require('./routes/task.routes');
-const app = express();
+require('dotenv').config();
 
 app.use(cors({
         origin: "https://chipper-torte-8a0d77.netlify.app",
