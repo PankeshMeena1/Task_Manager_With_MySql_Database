2366447a371975a1383af22fcd4f7f5e791cecaa
Pankesh Meena <pankeshmeena977@gamil.com>
2024-07-30 02:17:54 +0530

    add

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ const cors = require('cors');
 const userRoutes = require('./routes/user.routes');
 const taskRoutes = require('./routes/task.routes');
 const app = express();
+
 app.use(cors({
         origin: "https://chipper-torte-8a0d77.netlify.app",
         methods: "GET, POST, PUT, DELETE",
         credentials: true
 })); 
-
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use('/api/users', userRoutes);
